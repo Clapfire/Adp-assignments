808bca6e39256406b48328803354545f2f2aab48
Clapfire <klaur16@student.sdu.dk>
2018-02-08 22:38:43 +0100

    Created project folder and class Box with empty functions.

---
+++ Assignment 4/Assignment 4/Exercise 4.cpp
@@ -0,0 +1,51 @@
+// Exercise 4
+//
+// Make a class Box with length, width and height as private members. Make a constructor with
+// 3 parameters for initializing these three member - variables. Make the public member - functions
+// get_volume, get_surface. You can also make set - and get - member - functions for your 3
+// member - variables. In your main - function make some boxes and try them out.
+
+
+class Box 
+{
+public:
+
+	Box(int l, int w, int h) : length(l), width(w), height(h) {};
+
+	int get_volume(void) {
+
+	}
+
+	int get_surface(void) {
+
+	}
+
+	void set_lenght(int l) {
+
+	}
+
+	int get_lenght(void) {
+
+	}
+
+	void set_width(int w) {
+
+	}
+
+	int get_width(void) {
+
+	}
+
+	void set_height(int h) {
+
+	}
+
+	int get_height(void) {
+
+	}
+
+private:
+
+	int lenght, width, height;
+
+};
