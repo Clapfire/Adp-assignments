1d4c121552979d3f77ecf33d4023ee424d272b72
Clapfire <klaur16@student.sdu.dk>
2018-02-08 21:17:10 +0100

    Adder class

---
+++ Assignment 2/Assignment 2/Exercise 2.cpp
@@ -0,0 +1,6 @@
+//Exercise 2
+//
+//Make a class Adder. It has a private integer variable sum for holding the sum.
+//It has 3 public member - functions. The member - function add(int n) increases the sum. The
+//member - function reset_sum() sets sum to 0. The member - function get_sum() returns the sum.
+//Declare some adder objects and try them out in your main - function.#include <iostream>using namespace std;class Adder{public:	Adder(int n) : sum(n) {};	void add(int n) {		sum += n;	}	void reset_sum(void) {		sum = 0;	}	int get_sum(void) {		return sum;	}private:	int sum};
