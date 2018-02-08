9c994c07e229a57e5a6d127bd875c58cf32d39a7
Clapfire <klaur16@student.sdu.dk>
2018-02-08 23:00:15 +0100

    Created function definitions, an instance of a box and tested all functions with results in console.

--- Assignment 4/Assignment 4/Exercise 4.cpp
+++ Assignment 4/Assignment 4/Exercise 4.cpp
@@ -5,6 +5,8 @@
 // get_volume, get_surface. You can also make set - and get - member - functions for your 3
 // member - variables. In your main - function make some boxes and try them out.
 
+#include <iostream>
+using namespace std;
 
 class Box 
 {
@@ -54,6 +56,32 @@ private:
 
 };
 
-void main(void) {
+Box box1(5, 7, 10);
 
+int main(void) {
+
+	cout << "Box dimensions: " 
+		 << "length " << box1.get_length() 
+		 << " width " << box1.get_width() 
+		 << " height " << box1.get_height()
+		 << "\n"
+		 << "Box volume: " << box1.get_volume() << "\n"
+		 << "Box surface area: " << box1.get_surface() << "\n";
+
+	cout << "Changing length and width \n\n";
+
+	box1.set_length(15);
+	box1.set_width(2);
+
+	cout << "Box dimensions: "
+		<< "length " << box1.get_length()
+		<< " width " << box1.get_width()
+		<< " height " << box1.get_height()
+		<< "\n"
+		<< "Box volume: " << box1.get_volume() << "\n"
+		<< "Box surface area: " << box1.get_surface() << "\n";
+
+	std::getchar();
+
+	return 0;
 }
