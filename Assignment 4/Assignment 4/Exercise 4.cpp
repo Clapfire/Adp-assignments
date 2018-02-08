493d925826e4ec4c75e0e54ac707161663c9afcd
Clapfire <klaur16@student.sdu.dk>
2018-02-08 22:44:57 +0100

    Finished Box class definition

--- Assignment 4/Assignment 4/Exercise 4.cpp
+++ Assignment 4/Assignment 4/Exercise 4.cpp
@@ -13,39 +13,47 @@ public:
 	Box(int l, int w, int h) : length(l), width(w), height(h) {};
 
 	int get_volume(void) {
-
+		return length * width * height;
 	}
 
 	int get_surface(void) {
-
+		return (
+			(2 * width * length)
+			+ (2 * width * height)
+			+ (2 * length * height)
+			);
 	}
 
-	void set_lenght(int l) {
-
+	void set_length(int l) {
+		length = l;
 	}
 
-	int get_lenght(void) {
-
+	int get_length(void) {
+		return length;
 	}
 
 	void set_width(int w) {
-
+		width = w;
 	}
 
 	int get_width(void) {
-
+		return width;
 	}
 
 	void set_height(int h) {
-
+		height = h;
 	}
 
 	int get_height(void) {
-
+		return height;
 	}
 
 private:
 
-	int lenght, width, height;
+	int length, width, height;
 
 };
+
+void main(void) {
+
+}
