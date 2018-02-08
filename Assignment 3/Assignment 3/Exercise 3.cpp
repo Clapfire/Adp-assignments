31b24057d115faad13b910f9404a85a3076a5fdd
Clapfire <klaur16@student.sdu.dk>
2018-02-08 22:25:58 +0100

    Added functions, instantiated two instances of Account and tested all functions with output in console.

--- Assignment 3/Assignment 3/Exercise 3.cpp
+++ Assignment 3/Assignment 3/Exercise 3.cpp
@@ -6,30 +6,56 @@
 #include <iostream>
 using namespace std;
 
-class Account 
+class Account
 {
 public:
-	
-	Account(int n) : saldo(0), interest_rate(n) {};
 
-	void deposit(int n) {
+	Account(float n) : saldo(0), interest_rate(n) {};
 
+	void deposit(float n) {
+		saldo += n;
 	}
 
-	void withdraw(int n) {
-
+	void withdraw(float n) {
+		saldo -= n;
 	}
 
 	void add_interest(void) {
-
+		if (saldo > 0) {
+			saldo += (interest_rate / 100) * saldo;
+		}
 	}
 
-	int get_saldo(void) {
-
+	float get_saldo(void) {
+		return saldo;
 	}
 
 private:
-	
-	int saldo;
-	int interest_rate;
+
+	float saldo;
+	float interest_rate;
 };
+
+Account bob(5), george(10);
+
+void main(void) {
+
+	cout << "Bob Saldo: " << bob.get_saldo() << " George Saldo: " << george.get_saldo() << "\n";
+
+	bob.deposit(500);
+
+	cout << "Deposit 500 into Bob account \n";
+	cout << "Bob Saldo: " << bob.get_saldo() << "\n";
+
+	bob.add_interest();
+
+	cout << "Calculate interest for Bob's account \n";
+	cout << "Bob Saldo: " << bob.get_saldo() << "\n";
+
+	bob.withdraw(200);
+
+	cout << "Withdraw 200 from Bob's account \n";
+	cout << "Bob Saldo: " << bob.get_saldo() << "\n";
+
+	std::getchar();
+}
