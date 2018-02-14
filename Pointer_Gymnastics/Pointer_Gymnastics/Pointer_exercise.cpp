01b0329fd25c50e1e64c9d64068db85d120c3a92
Kasper Laursen <kassen-kasper@hotmail.com>
2018-02-14 14:45:29 +0100

    Added project folder and initial variables.

---
+++ Pointer_Gymnastics/Pointer_Gymnastics/Pointer_exercise.cpp
@@ -0,0 +1,25 @@
+/*
+Make a program with these integer variables : sum, a =2, b=3, c=4, and declare 3 pointer to int
+variables a_ptr, b_ptr, c_ptr; - a_ptr points at a etc.
+Your program must at the end output your variables in a table ( an “excerpt” of the memory ) sketched
+out below for the variables defined so far. Use the function setw from the iomanip library(add:
+#include<iomanip> in the top of your program) to align the columns – for instance assigning 10 spaces
+for each column like this for the headline
+cout << setw(10) << “ name “ << setw(10) << “ address “ << setw(10) << “ content “ << endl;
+and the entry in the table for a looks like this:
+cout << setw(10) << “ a “ << setw(10) << &a << setw(10) << a << endl;
+*/
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+void main(void) {
+	int sum, a = 2, b = 3, c = 4;
+	int *a_ptr = &a, *b_ptr = &b, *c_ptr = &c;
+
+
+
+	std:getchar();
+}
+
