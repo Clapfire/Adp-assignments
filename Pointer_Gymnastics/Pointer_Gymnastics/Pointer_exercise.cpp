fd96433168dde3b17f0d68ca76b2efe55780de66
Kasper Laursen <kassen-kasper@hotmail.com>
2018-02-14 15:21:10 +0100

    Completed task b. Use the built-in function new to make an integer location in memory and let the pointer variable int_ptr point at it. Give it the value 5.

--- Pointer_Gymnastics/Pointer_Gymnastics/Pointer_exercise.cpp
+++ Pointer_Gymnastics/Pointer_Gymnastics/Pointer_exercise.cpp
@@ -8,6 +8,26 @@ for each column like this for the headline
 cout << setw(10) << “ name “ << setw(10) << “ address “ << setw(10) << “ content “ << endl;
 and the entry in the table for a looks like this:
 cout << setw(10) << “ a “ << setw(10) << &a << setw(10) << a << endl;
+
+a)
+By using only the 3 pointer variables a_ptr, b_ptr and c_ptr calculate the sum of a, b and c and assign
+it to the variable sum.
+b)
+Use the built-in function new to make an integer location in memory and let the pointer variable int_ptr
+point at it. Give it the value 5.
+c)
+Make a reference to the variable a called aa. Set aa to 7 and watch what happens to the value of a in
+your table.
+e)
+Declare two pointers to int and let z_ptr point at a and y_ptr at aa. What can you say about the
+addresses they contain?
+f)
+Make a pointer to pointer to int: int_ptr_ptr. Let it point to a_ptr. Use it to assign 9 to a.
+Bonus round: Dynamic arrays
+Make a program, which asks the user about how many numbers to input. Then use the built-in function
+new to allocate the necessary number of members in a dynamic array ( int *dynarr = int[n];). Input the
+numbers to the dynamic array and then output them in reverse order. Delete the array with the function
+delete (delete dynarr[];) and let the program start all over again.
 */
 
 #include<iostream>
@@ -19,6 +39,8 @@ void main(void) {
 	int *a_ptr = &a, *b_ptr = &b, *c_ptr = &c;
 
 	sum = (*a_ptr + *b_ptr + *c_ptr);
+	
+	int *int_ptr = new int[5];
 
 	cout << setw(10) << " name " << setw(10) << " address " << setw(10) << " content " << endl;
 	cout << setw(10) << " a " << setw(10) << &a << setw(10) << a << endl;
@@ -28,6 +50,7 @@ void main(void) {
 	cout << setw(10) << " a_ptr " << setw(10) << &a_ptr << setw(10) << a_ptr << endl;
 	cout << setw(10) << " b_ptr " << setw(10) << &b_ptr << setw(10) << b_ptr << endl;
 	cout << setw(10) << " c_ptr " << setw(10) << &c_ptr << setw(10) << c_ptr << endl;
+	cout << setw(10) << " int_ptr " << setw(10) << &int_ptr << setw(10) << int_ptr << endl;
 
 	std:getchar();
 }
