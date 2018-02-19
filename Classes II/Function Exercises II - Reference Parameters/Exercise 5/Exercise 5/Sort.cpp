4415ea9c820188fab81d2f68a2b618e915a213b9
Clapfire <klaur16@student.sdu.dk>
2018-02-19 23:28:38 +0100

    Set up functions for Exercise 5

---
+++ Classes II/Function Exercises II - Reference Parameters/Exercise 5/Exercise 5/Sort.cpp
@@ -0,0 +1,22 @@
+/*
+Exercise 5.
+Make a program, which inputs 3 numbers.
+Make a void function called sort with 3 reference parameters. It sorts the values. If we have a = 5, b =
+1, c = 3 then after the call sort(a,b,c) we have a = 1, b = 3, c = 5. Output the numbers.
+(hint: you could make use of the function you defined in the previous exercise)
+*/
+
+#include <iostream>
+using namespace std;
+
+void sort(int& a, int& b, int& c);
+
+int main(void) {
+
+
+	return 0;
+}
+
+void sort(int& a, int& b, int& c) {
+
+}
